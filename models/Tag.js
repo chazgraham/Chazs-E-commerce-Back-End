d2185bdf22a1d75b3f7cd8708eebcead10abf606
chaz Graham <chazhg80.cg@gmail.com>
2022-10-11 14:56:50 -0500

    all routes for categories finished

--- models/Tag.js
+++ models/Tag.js
@@ -8,7 +8,9 @@ Tag.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     tag_name: {
       type: DataTypes.STRING
--- models/index.js
+++ models/index.js
@@ -4,27 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-});
-
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
+// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tag',
   foreignKey: 'tag_id'
 });
 
