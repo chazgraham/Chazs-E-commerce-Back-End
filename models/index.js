aa1b536c62c3e946a2cf5ed7161bd1bbd21a082d
chaz Graham <chazhg80.cg@gmail.com>
2022-10-11 13:10:49 -0500

    database index for modles complete

--- models/index.js
+++ models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tag',
+  foreignKey: 'tag_id'
+});
 
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+
+
 
 module.exports = {
   Product,
